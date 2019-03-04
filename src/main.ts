7c5faea940807e8b3cad5f0b6a249bc9519a44d8
terrygonguet <terry@gonguet.com>
2019-03-04 22:12:59 +0100

    Switch to Vue PWA

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,13 @@
-import * as express from "express";
-import * as logger from "morgan";
-import YT from "./yt";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-if (!process.env.PORT) process.env.PORT = "8080";
+Vue.config.productionTip = false;
 
-const app = express();
-const yt = new YT();
-
-app.use(logger("dev"));
-app.use(express.static("static"));
-
-app.get("/api/videos", (req, res) => {
-  res.json(yt.videos);
-});
-
-app.get("/api/update", (req, res) => {
-  yt.update().then(() => yt.saveState());
-  res.json("ok");
-});
-
-app.get("/api/removevideo/:index", (req, res) => {
-  let index = req.params.index;
-  res.json(yt.remove(index));
-  yt.saveState();
-});
-
-app.get("/api/setcookie/:cookie", (req, res) => {
-  let cookie = req.params.cookie;
-  yt.cookie = cookie;
-  res.json("ok");
-  yt.saveState();
-});
-
-app.listen(process.env.PORT, () =>
-  console.log("Listening on port " + process.env.PORT)
-);
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
